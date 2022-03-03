fcb79bd940795b27b7eb2ade706c68d50b9322da
bitsizelearning <bitesize.learning22@gmail.com>
2022-03-03 10:38:12 +0700

    Add get and post menu routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
-const express = require('express');
+import express from 'express';
 
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
+const menus = [
+  { name: 'Vanilla', description: 'A plain vanilla ice cream' },
+  { name: 'Choco Chips', description: 'A chocolate chips ice cream' },
+];
+
 app.get('/', (req, res, next) => {
   res.send('Hello, World!');
+});
+
+app.get('/goodbye', (req, res, next) => {
+  res.send('Goodbye, World!');
+});
+
+app.get('/menu', (req, res, next) => {
+  res.json({
+    data: menus,
+  });
+});
+
+app.post('/menu', (req, res, next) => {
+  menus.push(req.body);
+  res.json({
+    message: 'Success!',
+  });
 });
 
 app.listen(port, () => {
